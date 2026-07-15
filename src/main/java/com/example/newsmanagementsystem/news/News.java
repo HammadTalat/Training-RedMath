8f19a74b7cd2e962be103467500a25deef3eaa57
Hammad Talat <hammad.baig874@gmail.com>
2026-07-15 16:12:16 +0500

    Created news entity, configured liquibase and property file for mysql connection

---
+++ src/main/java/com/example/newsmanagementsystem/news/News.java
@@ -0,0 +1,82 @@
+package com.example.newsmanagementsystem.news;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "news")
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id")
+    private Long newsId;
+
+
+    private String title;
+
+
+    private String details;
+
+    @Column(name = "reported_by")
+    private String reportedBy;
+
+    @Column(name = "reported_at", updatable = false)
+    private LocalDateTime reportedAt;
+
+    public News() {
+    }
+
+    @PrePersist
+    private void assignReportedAt() {
+        reportedAt = LocalDateTime.now();
+    }
+
+    public Long getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(Long newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getReportedBy() {
+        return reportedBy;
+    }
+
+    public void setReportedBy(String reportedBy) {
+        this.reportedBy = reportedBy;
+    }
+
+    public LocalDateTime getReportedAt() {
+        return reportedAt;
+    }
+
+    public void setReportedAt(LocalDateTime reportedAt) {
+        this.reportedAt = reportedAt;
+    }
+}
