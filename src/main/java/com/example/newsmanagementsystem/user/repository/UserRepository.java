8ce1a97c3184500a0cfdddb169334f5cba3eaf4d
Hammad Talat <hammad.baig874@gmail.com>
2026-07-21 15:57:52 +0500

    Successfully implemented login with github and google now both are supported in our application along with opaque token implementation

--- src/main/java/com/example/newsmanagementsystem/user/repository/UserRepository.java
+++ src/main/java/com/example/newsmanagementsystem/user/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package com.example.newsmanagementsystem.user.repository;
 
 import com.example.newsmanagementsystem.user.entity.AppUser;
+import com.example.newsmanagementsystem.user.entity.AuthProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<AppUser, Long> {
-
+    Optional<AppUser> findByAuthProviderAndProviderUserId(
+            AuthProvider authProvider,
+            String providerUserId
+    );
     Optional<AppUser> findByUsernameIgnoreCase(String username);
 
     Optional<AppUser> findByAccessToken(String token);
